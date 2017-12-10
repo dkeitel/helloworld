508182fdabe9f74bd274039f2b0197601779aec6
David Keitel <david@keitel.ca>
2017-12-10 15:52:37 -0800

    change message one more time

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	log.Println("Starting helloworld application...")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello David!\n")
+		fmt.Fprintf(w, "Hello David!!\n")
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
